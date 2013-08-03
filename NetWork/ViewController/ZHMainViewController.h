ae8c6aded1cbd7fe48dfd7d9ec920e4e983f4218
zhzenghui <zhzenghui@gmail.com>
2013-08-03 15:21:29 +0800

    添加mian文件

---
+++ NetWork/ViewController/ZHMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZHMainViewController.h
+//  NetWork
+//
+//  Created by zeng hui on 13-8-3.
+//  Copyright (c) 2013年 zeng hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHMainViewController : UIViewController
+
+@end
---
+++ NetWork/ViewController/ZHMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZHMainViewController.m
+//  NetWork
+//
+//  Created by zeng hui on 13-8-3.
+//  Copyright (c) 2013年 zeng hui. All rights reserved.
+//
+
+#import "ZHMainViewController.h"
+
+@interface ZHMainViewController ()
+
+@end
+
+@implementation ZHMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
